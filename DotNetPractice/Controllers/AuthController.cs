e6b0a12d0ea3bd767da8c4fe0cd5d48cbfbdf35f
Aakarshan Gandhi <gandhaak@gmail.com>
2018-09-20 14:30:10 -0400

    v 1.1
    Login Authentication - status - up

---
+++ DotNetPractice/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+using System.Threading.Tasks;
+using DotNetPractice.Data;
+using DotNetPractice.DTOS;
+using DotNetPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DotNetPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        //register method
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserToRegisterDto userToRegisterDto){
+            userToRegisterDto.Username = userToRegisterDto.Username.ToLower();
+
+            if(await _repo.UserExists(userToRegisterDto.Username))
+                return BadRequest("Username already exists !");
+            
+            var userToCreate = new User{
+                Username = userToRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userToRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+        //login method
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto){
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new []{
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor(){
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(0.5),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+    }
+}
---
+++ DotNetPractice/DTOS/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+namespace DotNetPractice.DTOS
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }    
+        public string Password { get; set; }
+    }
+}
